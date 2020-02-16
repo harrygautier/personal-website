ba0769a41cd014ddd3454b490cbe3e6c6325cd1b
Harry <harrygautierdesign@gmail.com>
2020-02-16 21:35:23 +0000

    importing bootstrap and scss changes

--- wp-content/themes/hghome/front-page.php
+++ wp-content/themes/hghome/front-page.php
@@ -1,46 +1,29 @@
 <!-- home page template -->
 <?php get_header(); ?>
 
-<section class="about">
-  <div class="container my-1 text-left">
-
+<section id="hero">
+  <div class="container text-left pt-6 pb-4">
+    <!--
     <h1 class="about-title text-blue pt-6">Hi, I'm Harry Gautier.</h1>
   	<p class="about-description mt-3">I'm a freelance designer specialising in <strong>brand identity</strong> &amp; <strong>web design</strong>. Based in Chester, I work with clients directly to help create a stronger looking brand and robust online presence.</p>
   	<small class="about-quote text-grey-2">Art is created. Design is crafted.</small>
   	<img class="about-signature my-3" src="<?php bloginfo('stylesheet_directory'); ?>/assets/brand/hg_signature_blue.png">
+    -->
+
+    <h1 class="text-white hero-title">Developer & Designer</h1>
+    <p class="about-description mt-3 text-light">A web developer with a strong foundation in design.</p>
+
 
   </div>
 </section>
 
 
-<section class="work">
-	<div class="row mx-auto">
-		<?php
 
-		$args = array(
-			'post_type' => 'post',
-			'post_status' => 'publish',
-			'orderby' => 'date',
-			'order'   => 'DESC',
-			'posts_per_page' => 6,
-		);
-		$arr_posts = new WP_Query( $args );
+<?php get_template_part("template-parts/component", "work"); ?>
 
-		if ( $arr_posts->have_posts() ) {
+<?php get_template_part("template-parts/component", "about"); ?>
 
-			while ( $arr_posts->have_posts() ) {
-				$arr_posts->the_post();
-
-				get_template_part('template-parts/posts/content', 'primary');
-			}
-		}
-
-		wp_reset_postdata();
-		?>
-</section>
-
-
-<?php get_template_part("template-parts/component", "my-skills"); ?>
+<?php get_template_part("template-parts/component", "cv"); ?>
 
 
 <!-- gets the content for this page -->
--- wp-content/themes/hghome/header.php
+++ wp-content/themes/hghome/header.php
@@ -44,10 +44,10 @@
 
 
 		if ( is_front_page()/*or is_home()*/ ) {
-			$page_classes = array('home-class', 'another-class', 'bg-white', 'text-dark');
+			$page_classes = array('home-class', 'another-class', 'bg-dark', 'text-white');
 		}
 		else {
-			$page_classes = array('not-home-class', 'bg-white', 'text-dark');
+			$page_classes = array('not-home-class', 'bg-dark', 'text-white');
 		}
 
 	?>
@@ -55,41 +55,14 @@
 	<body <?php body_class($page_classes); //array passes in whatever class ?>>
 		<header>
 
-			<nav class="navbar fixed-top navbar-expand-sm text-center">
+			<nav class="navbar fixed-top navbar-expand-sm text-center bg-dark text-white">
 				<div class="container-fluid">
 
 					<a class="navbar-brand" href="<?php echo get_home_url(); ?>">
-						<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/brand/hg_logo_web_blue.png" class="header-logo">
-					</a>
-
-					<div class="form-inline ml-auto">
-						<div class="contact-icons d-flex flex-row justify-content-center align-content-center">
-							<div class="animate animate-up mx-2">
-								<a href="tel:+447902819990" target="_blank">
-									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512"><path d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z" class=""></path></svg>
-								</a>
-							</div>
-							<div class="animate animate-up mx-2">
-								<a href="mailto:harrygautierdesign@gmail.com" target="_blank">
-									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512"><path d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z" class=""></path></svg>
-								</a>
-							</div>
-							<div class="animate animate-up mx-2">
-								<a class="border-bottom border-dark" type="button" name="download-cv">Download CV</a>
-							</div>
-						</div>
-
-					</div>
-
-				</div>
-			</nav>
+						<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/brand/hg_logo_web_white.png" class="header-logo">
 
 
-			<nav class="navbar fixed-top navbar-expand-sm text-center" style="top: 100px!important;">
-				<div class="container-fluid">
 
-					<a class="navbar-brand" href="<?php echo get_home_url(); ?>">
-						<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/brand/hg_logo_web_blue.png" class="header-logo">
 					</a>
 
 
@@ -100,9 +73,9 @@
 							<a class="nav-item ml-auto mr-2 text-hide">Menu</a>
 
 							<div class="nav-toggler">
-								<span class="icon-bar top-bar blue-bar"></span>
-								<span class="icon-bar middle-bar blue-bar"></span>
-								<span class="icon-bar bottom-bar blue-bar"></span>
+								<span class="icon-bar top-bar bg-white"></span>
+								<span class="icon-bar middle-bar bg-white"></span>
+								<span class="icon-bar bottom-bar bg-white"></span>
 								<span class="sr-only">Menu</span>
 							</div>
 						</div>
@@ -115,29 +88,35 @@
 						<div class="form-inline">
 							<!-- css classes can be added in the menus section on wordpress-->
 							<?php
-								wp_nav_menu(array(
-									'theme_location'=>'primary',
-									'container'=> false,
-									'menu_class'=>'navbar-nav mx-auto')
-								);
+								// wp_nav_menu(array(
+								// 	'theme_location'=>'primary',
+								// 	'container'=> false,
+								// 	'menu_class'=>'navbar-nav mx-auto text-white')
+								// );
 							?>
 
+							<ul class="navbar-nav mx-auto">
+								<li class="nav-item mx-2 my-2 my-sm-0"><a href="#Work" class="nav-link">Work</a></li>
+								<li class="nav-item mx-2 my-2 my-sm-0"><a href="#About" class="nav-link">About</a></li>
+								<li class="nav-item mx-2 my-2 my-sm-0"><a href="#CV" class="nav-link">CV</a></li>
+							</ul>
+							<!--
 							<div class="contact-icons d-flex flex-row justify-content-center align-content-center">
 								<div class="animate animate-up mx-2">
 									<a href="tel:+447902819990" target="_blank">
-										<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512"><path d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z" class=""></path></svg>
+										<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512" class="fill-white"><path d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z" class=""></path></svg>
 									</a>
 								</div>
 								<div class="animate animate-up mx-2">
 									<a href="mailto:harrygautierdesign@gmail.com" target="_blank">
-										<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512"><path d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z" class=""></path></svg>
+										<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512" class="fill-white"><path d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z" class=""></path></svg>
 									</a>
 								</div>
 								<div class="animate animate-up mx-2">
 									<button class="btn btn-dark" type="button" name="download-cv">Download CV</button>
 								</div>
 							</div>
-
+						-->
 						</div>
 					</div>
 				</div>
