c7e1195cdd20e87be37336e95a61c8d1e4c90f26
Harry <harrygautierdesign@gmail.com>
2020-02-19 22:28:24 +0000

    layout and animation changes

--- wp-content/themes/hghome/single.php
+++ wp-content/themes/hghome/single.php
@@ -25,60 +25,58 @@ if (have_posts()) {
 				<h1><?php the_title(); ?></h1>
 			</div>
 
-			<figure class="p-relative overflow-hidden p-0 px-xl-4 pt-4 m-4 mb-4 off-screen off-screen-bottom">
+			<figure class="overflow-hidden p-0 px-xl-4 pt-4 m-4 mb-4 off-screen off-screen-bottom">
 				<img src="<?php if ( has_post_thumbnail() ) { the_post_thumbnail_url(); }?>" class="position-block">
+
 			</figure>
 			<?php
-			if ($website_link) {
-			?>
+			if ($website_link) { ?>
+				<div class="container-fluid off-screen off-screen-left">
+					<div class="website-link animate animate-left text-right pr-4 mx-xl-4 mb-5">
+						<a class="border-bottom border-light pb-1" target="_blank" href="<?php echo $website_link; ?>">Visit Website</a>
+					</div>
+				</div>
+			<?php }
 
-			<?php } ?>
-
-			<?php
 			//only shows brief and tags if brief exists
 			if($brief) { ?>
-			<div class="container-fluid off-screen off-screen-left">
-				<div class="website-link animate animate-left text-right pr-4 mx-xl-4 mb-5">
-					<a class="border-bottom border-light pb-1" target="_blank" href="<?php echo $website_link; ?>">Visit Website</a>
-				</div>
-			</div>
 
-			<div class="intro container mx-auto mb-5">
+				<div class="intro container mx-auto mb-5">
 
-				<div class="row d-flex flex-row flex-md-row-reverse justify-content-center">
+					<div class="row d-flex flex-row flex-md-row-reverse justify-content-center my-6 off-screen off-screen-right">
 
-					<div class="col-12 col-md-8 col-lg-6">
-						<h2 class="mb-0">Brief</h2>
-						<div class="excerpt">
-							<p> <?php	echo $brief; ?>
-							</p>
+						<div class="col-12 col-md-8 col-lg-6">
+							<h2 class="mb-0">Project</h2>
+							<div class="excerpt">
+								<p> <?php	echo $brief; ?>
+								</p>
+							</div>
 						</div>
-					</div>
 
-					<div class="col-12 col-md-4 col-lg-3">
+						<div class="col-12 col-md-4 col-lg-3">
 
-						<div class="tags mt-md-5 pt-3">
-							<?php
-							$posttags = get_the_tags();
-							if ($posttags) {
-								echo '<ul class="p-0 list-unstyled">';
-								foreach($posttags as $tag) {
-									echo '<li><h4>' .$tag->name. '</h4></li>';
+							<div class="tags mt-md-5 pt-3">
+								<?php
+								$posttags = get_the_tags();
+								if ($posttags) {
+									echo '<ul class="p-0 list-unstyled">';
+									foreach($posttags as $tag) {
+										echo '<li><h4>' .$tag->name. '</h4></li>';
+									}
+									echo '</ul>';
 								}
-								echo '</ul>';
-							}
-							?>
+								?>
 
+							</div>
 						</div>
+
 					</div>
 
-				</div>
+
+					</div>
 
 
 				</div>
-
-
-			</div>
 			<?php } ?>
 
 
