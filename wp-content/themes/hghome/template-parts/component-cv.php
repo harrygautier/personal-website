37f90d005095ed1fec71d2a4dbc8c0de46ce819d
Harry <harrygautierdesign@gmail.com>
2020-02-19 00:10:38 +0000

    creating digital cv

--- wp-content/themes/hghome/template-parts/component-cv.php
+++ wp-content/themes/hghome/template-parts/component-cv.php
@@ -1,49 +1,77 @@
-<section class="bg-dark text-white pt-6">
+<section class="cv py-6">
 	<div id="cv" class="link-anchor"></div>
 
 	<div class="container-fluid">
+
 		<div class="row justify-content-center mx-auto">
 
-			<div class="col-12 col-md-10">
+			<div class="col-12 col-md-10 off-screen off-screen-bottom">
 				<h2 class="pb-2">CV</h2>
-				<a class="animate animate-right" href="#">PDF Download</a>
+				<div class="animate-animate-right">
+					<a href="#">PDF Download</a>
+				</div>
 			</div>
+		</div>
 
-			<div class="col-12 col-md-10 mt-5">
-				<h3>Key Skills</h3>
-			</div>
-
-			<div class="col-12 col-md-10">
+		<div class="row justify-content-center mx-auto">
+			<div class="col-12 col-lg-3 col-xl-4 off-screen off-screen-bottom">
+				<h3 class="mt-5 mb-4">Key Skills</h3>
 				<div class="row">
-					<div class="col-12 col-md-6">
+					<div class="col-12 col-sm-6 col-lg-12 col-xl-6">
 						<h4>Technologies</h4>
-						<ul>
+						<ul class="list-unstyled mb-3">
 							<li>Adobe CC Suite</li>
 							<li>WordPress</li>
 							<li>Bootstrap</li>
 						</ul>
-						<br>
 						<h4>Interpersonal</h4>
-						<p>Teamwork</p>
-						<p>Communication</p>
-						<p>Creativity</p>
+						<ul class="list-unstyled mb-3">
+							<li>Teamwork</li>
+							<li>Communication</li>
+							<li>Creativity</li>
+						</ul>
 					</div>
-					<div class="col-12 col-md-6">
 
+					<div class="col-12 col-sm-6 col-lg-12 col-xl-6">
+						<h4>Industry Knowledge</h4>
+						<ul class="list-unstyled mb-5">
+							<li>Graphic Design</li>
+							<li>Brand Identity</li>
+							<li>Typography</li>
+							<li>UI & UX Design</li>
+							<li>Full-Stack Web Development</li>
+							<li>Responsive Web Design</li>
+							<li>HTML, CSS, JS & PHP</li>
+							<li>Git & Github</li>
+						</ul>
 					</div>
 				</div>
 			</div>
 
+			<div class="col-12 col-lg-7 col-xl-6 off-screen off-screen-bottom">
+				<h3 class="mt-5 mb-4">Work Experience</h3>
+				<div class="mb-5">
+					<p class="mb-0"><strong>Graphic Design and PR... CANT THINK OF COMPANY, IT'll COME</strong></p>
+					<small class="mb-0">October 2015</small>
+					<p>Worked closely with the graphic design team with their social media presence and branding. Offered ideas and concepts for design projects that they were involved in internally.</p>
+				</div>
+				<div class="mb-5">
+					<p class="mb-0"><strong>Graphic Design and PR... CANT THINK OF COMPANY, IT'll COME</strong></p>
+					<small class="mb-0">October 2015</small>
+					<p>Worked closely with the graphic design team with their social media presence and branding. Offered ideas and concepts for design projects that they were involved in internally.</p>
+				</div>
+			</div>
+
+			<div class="col-12 col-md-10 off-screen off-screen-bottom">
+				<h3 class="my-4">Employment</h3>
+				<div class="mb-5">
+					<p class="mb-0"><strong>Attendance Officer - Christleton High School</strong></p>
+					<small class="mb-0">December 2018 - November 2019</small>
+					<p>After completing my A-Levels, I was asked to come back to work at Christleton High School with several duties within the office and art department. Working alongside both senior and other members of staff to ensure that my key responsibilities were fulfulled.</p>
+				</div>
+			</div>
 
 		</div>
-	</div>
-
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
+	</div><!-- .container-fluid -->
 
 </section>
